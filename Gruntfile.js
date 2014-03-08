9405c8ea6ed0ba6ac381cefc3f481961cc4326f2
Shark <xfcjscn@gmail.com>
2014-03-09 01:01:03 +0800

    dont rev requirejs loaded scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -223,7 +223,7 @@ module.exports = function (grunt) {
 			dist: {
 				files: {
 					src: [
-						'<%= yeoman.dist %>/scripts/{,*/}*.js',
+						'<%= yeoman.dist %>/scripts/*.js',
 						'<%= yeoman.dist %>/styles/{,*/}*.css',
 						'<%= yeoman.dist %>/images/{,*/}*.{gif,jpeg,jpg,png,webp}',
 						'<%= yeoman.dist %>/styles/fonts/{,*/}*.*'
