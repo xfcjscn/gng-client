bffc368a16cda86d1b2b55cbedb539fed335965c
Shark <xfcjscn@gmail.com>
2014-03-17 12:36:10 +0800

    support remote test

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,7 +118,7 @@ module.exports = function (grunt) {
 				port: 80,
 				livereload: 35729,
 				// Change this to '0.0.0.0' to access the server from outside
-				hostname: 'localhost'
+				hostname: '0.0.0.0'
 			},
 			livereload: {
 				options: {
