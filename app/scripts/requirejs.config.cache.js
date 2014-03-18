94f8055eccfd8e3e97aeb3f85e870a0e8af4900e
Shark <xfcjscn@gmail.com>
2014-03-18 22:39:48 +0800

    upgrade infra

--- app/scripts/requirejs.config.cache.js
+++ app/scripts/requirejs.config.cache.js
@@ -1,6 +1,3 @@
-define(['infra-client/app/scripts/models/angular', 'infra-client/app/scripts/models/infra', 'css!infra-client/app/styles/infra', 'css!gng-client/app/styles/gplatform', 'q/q', 'purl/purl', 'plupload/js/i18n/zh_CN'], function () {
+define(['infra-client/app/scripts/requirejs.config.cache', 'css!gng-client/app/styles/gplatform', 'plupload/js/i18n/zh_CN'], function () {
 	return {};
 });
-
-//Can not include ./vendor/jaydata/jaydata, as there will be path issue: /angular_comp/GPlatform/jaydataproviders/oDataProvider.js
-//Can not include './models/server' as it rely on jaydata
